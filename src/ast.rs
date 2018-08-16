0ad957bf24a275d3e190e9dc3199e04c16955a76
Erin <sylphofelectricity@gmail.com>
2018-08-15 22:25:39 -0500

    cargo fmt

--- src/ast.rs
+++ src/ast.rs
@@ -16,24 +16,24 @@ impl Ident {
 
 #[derive(Debug)]
 pub enum Literal {
-    Integer(u64)
+    Integer(u64),
 }
 
 /// Port directionality.
 #[derive(Debug, Display)]
 pub enum Dir {
-    #[strum(to_string="in")]
+    #[strum(to_string = "in")]
     In,
-    #[strum(to_string="out")]
+    #[strum(to_string = "out")]
     Out,
 }
 
 /// Edge side (rising/+ or falling/-).
 #[derive(Debug, Display)]
 pub enum Edge {
-    #[strum(to_string="posedge")]
+    #[strum(to_string = "posedge")]
     Rising,
-    #[strum(to_string="negedge")]
+    #[strum(to_string = "negedge")]
     Falling,
 }
 
@@ -73,46 +73,46 @@ pub struct BinaryOp {
 /// Operators with arity two.
 #[derive(Debug, Display)]
 pub enum BinaryOpTy {
-    #[strum(to_string="+")]
+    #[strum(to_string = "+")]
     Add,
-    #[strum(to_string="-")]
+    #[strum(to_string = "-")]
     Sub,
-    #[strum(to_string="*")]
+    #[strum(to_string = "*")]
     Mul,
-    #[strum(to_string="/")]
+    #[strum(to_string = "/")]
     Div,
 
-    #[strum(to_string="==")]
+    #[strum(to_string = "==")]
     Eq,
-    #[strum(to_string="!=")]
+    #[strum(to_string = "!=")]
     Neq,
-    #[strum(to_string="<")]
+    #[strum(to_string = "<")]
     Lt,
-    #[strum(to_string=">")]
+    #[strum(to_string = ">")]
     Gt,
-    #[strum(to_string="<=")]
+    #[strum(to_string = "<=")]
     Lte,
-    #[strum(to_string=">=")]
+    #[strum(to_string = ">=")]
     Gte,
 
-    #[strum(to_string="||")]
+    #[strum(to_string = "||")]
     Or,
-    #[strum(to_string="&&")]
+    #[strum(to_string = "&&")]
     And,
 
-    #[strum(to_string="|")]
+    #[strum(to_string = "|")]
     BitOr,
-    #[strum(to_string="&")]
+    #[strum(to_string = "&")]
     BitAnd,
-    #[strum(to_string="<<")]
+    #[strum(to_string = "<<")]
     LShift,
-    #[strum(to_string=">>")]
+    #[strum(to_string = ">>")]
     RShift,
 }
 
 /// Operators with arity one.
 #[derive(Debug, Display)]
 pub enum UnaryOpTy {
-    #[strum(to_string="!")]
+    #[strum(to_string = "!")]
     Not,
 }
